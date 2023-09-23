c62466f5bef8f3763e7894893ed9fb417a367b47
14sofficial <145863503+14sofficial@users.noreply.github.com>
2023-09-24 00:29:43 +0700

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,17 @@
+export const links = [
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/14s.official/",
+      image: "./assets/ig.png",
+    },
+    {
+      name: "Spotify",
+      link: "https://open.spotify.com/intl-id/artist/5Xfqlk3Fu7jhLnlc7Y5zkZ?si=2gMDrYcpQvaKXdsoEIqjcA",
+      image: "./assets/spotify.png",
+    },
+    {
+      name: "YouTube",
+      link: "https://www.youtube.com/@14s.official",
+      image: "./assets/yt.png",
+    },
+  ];
